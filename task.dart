4b7e3063077b0f6423b5538ee014e047559fcfb7
Aleksandra <82601472+ALewandowska-sys@users.noreply.github.com>
2022-04-04 18:35:13 +0200

    Create task.dart

---
+++ task.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Check extends StatefulWidget {
+  const Check({Key? key}) : super(key: key);
+
+  @override
+  State<Check> createState() => _MyCheck();
+}
+
+class _MyCheck extends State<Check> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
+class Task extends StatelessWidget{
+  final String task;
+  Task(this.task);
+  Widget build(BuildContext context){
+    return Container(
+      child: Row(
+          children: [
+            Check(),
+            Text(task)]
+      ),
+    );
+  }
+}
