b6aa02ecdb90d7263caaa5c45ec425c952242231
Aleksandra <82601472+ALewandowska-sys@users.noreply.github.com>
2022-05-05 21:26:08 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
 import './task.dart';
-
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,43 +12,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'To Do Homie',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blueGrey,
       ),
       home: MyAplication(),
     );
   }
 }
 
-
-class MyAplication extends StatefulWidget{
+class MyAplication extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StateAplication();
 }
 
-
-
-class StateAplication extends State<MyAplication>{
-
+class StateAplication extends State<MyAplication> {
   @override
-  Widget build(BuildContext centext){
+  Widget build(BuildContext centext) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('To do list'),
-        ),
-        body: Center(child: Text("You don't have a task to do")),
-        floatingActionButton: FloatingActionButton(
+      appBar: AppBar(
+        title: Text('To do list'),
+      ),
+      body: Center(
+        child: Text("You don't have a task to do"),
+      ),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
-              context: context,
-              builder: (context) => Container(
-                  height: 60, child: Text('This is a modal bottom sheet'),
-                  ),
-                  );
+            context: context,
+            builder: (context) => Container(
+              height: 100,
+              child: Text('This is a modal bottom sheet'),
+            ),
+          );
         },
         child: const Icon(Icons.add),
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.blue,
       ),
     );
   }
 }
-
