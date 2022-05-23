20676607806b2e93a6804922d2d231ca41db5135
Aleksandra <82601472+ALewandowska-sys@users.noreply.github.com>
2022-05-23 15:10:19 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'body.dart';
+import 'task.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To Do Homie',
+      title: 'To Do',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
@@ -20,18 +20,71 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAplication extends StatefulWidget {
+  const MyAplication({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => StateAplication();
 }
 
 class StateAplication extends State<MyAplication> {
+
+  TextEditingController taskController = TextEditingController();
+  List task = <String>[];
+
+
+
+  createBody(){
+    if(task.isEmpty){
+      return Center(
+        child: Opacity(
+          opacity: 0.4,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Icon(
+                Icons.list_alt,
+                size: 70,
+              ),
+              Text('Add some task',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            ],
+          ),
+        ),
+      );
+    }
+    else{
+      return Container(
+        padding: EdgeInsets.only(top:20, left: 10),
+        child: Column(
+            children: List.generate(task.length, (index)
+          {
+            return Task(task[index]);
+          },
+          ),
+          ),
+      );
+    }
+  }
+
+  addTask(){
+    setState(() {
+    task.add(taskController.text);
+    },);
+  }
+
+
+
+
+
   @override
-  Widget build(BuildContext centext) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To do list'),
+        centerTitle: true,
+        title: const Text('To do list', style: TextStyle(fontSize: 25, color: Colors.white),),
       ),
-      body: Body(),
+      body: createBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
@@ -40,21 +93,24 @@ class StateAplication extends State<MyAplication> {
               color: Colors.cyan[50],
               child: Column(
                 children: [
-                  const TextField(
-                    decoration: InputDecoration(
+                  TextField(
+                    controller: taskController,
+                    decoration: const InputDecoration(
                         border: InputBorder.none,
                         labelText: 'Enter Title',
                     ),
                   ),
-                  TextButton(onPressed: () {},
-                      child: Text("Add"),
+                  TextButton(onPressed: () {
+                    addTask();
+                  },
+                      child: const Text("Add"),
                   )
                 ],
               ),
             ),
           );
         },
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.white,),
         backgroundColor: Colors.cyan[600],
       ),
     );
