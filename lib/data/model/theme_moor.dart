2088dff912c02a489b3e71f3aa4564f4cf823381
ALewandowska-sys <ola.lewa109@gmail.com>
2022-11-12 15:15:52 +0100

    rename table

--- lib/data/model/theme_moor.dart
+++ lib/data/model/theme_moor.dart
@@ -1,7 +1,7 @@
 import 'package:moor/moor.dart';
 import 'package:moor_flutter/moor_flutter.dart';
 
-class SelectColors extends Table {
+class ThemeColors extends Table {
   IntColumn get id => integer().autoIncrement()();
   IntColumn get colorName => integer()();
   BoolColumn get selected => boolean().withDefault(const Constant(false))();
